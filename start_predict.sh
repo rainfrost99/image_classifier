bd88259c370f96155592a3c31817cab1bc928ff8
rainfrost99 <quaver23@gmail.com>
2020-08-21 23:23:57 +0800

    Add files via upload

---
+++ start_predict.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python predict.py --imagedir /home/workspace/ImageClassifier/flowers/test/100/image_07896.jpg --checkpointdir /home/workspace/ImageClassifier/checkpoint2.pth --gpu --kvalue 5
